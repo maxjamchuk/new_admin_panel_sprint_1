99e0741faf10d2dfb0b325db4a82f2fc9b7fc616
Maks Iamchuk <maks.iamchuk@outlook.com>
2023-04-18 18:16:45 +0300

    ADDED: new filters and fields in admin panel

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Filmwork
-from .models import Genre
-from .models import GenreFilmwork
-from .models import Person
-from .models import PersonFilmwork
+from django.utils.translation import gettext_lazy as _
+
+from .models import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
 
 
 class GenreFilmworkInline(admin.TabularInline):
@@ -26,13 +24,30 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
-    inlines = (GenreFilmworkInline, )
+    inlines = (GenreFilmworkInline, PersonFilmworkInline,)
 
-    list_display = ('title', 'type', 'creation_date', 'rating', )
-    list_filter = ('type', 'genres', )
-    search_fields = ('title', 'description', 'id', )
+    list_display = ('title', 'type', 'creation_date', 'rating', 'get_genres',)
+    list_filter = ('type', 'creation_date',)
+    search_fields = ('title', 'description', 'id',)
+    list_prefetch_related = ('genres',)
+
+    def get_queryset(self, request):
+        queryset = (
+            super()
+            .get_queryset(request)
+            .prefetch_related(*self.list_prefetch_related)
+        )
+        return queryset
+
+    def get_genres(self, obj):
+        return ', '.join([genre.name for genre in obj.genres.all()])
+
+    get_genres.short_description = _('filmwork_genre')
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = ('full_name', 'gender', 'created',)
+    list_filter = ('gender', 'created',)
+    search_fields = ('full_name', 'id',)
