e8242c6de1b1ec801ccf1739d73fc82fbe805ee6
Maks Iamchuk <maks.iamchuk@outlook.com>
2023-04-18 17:10:35 +0300

    FIXED: debug mode now works only with True flag

--- movies_admin/config/urls.py
+++ movies_admin/config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from settings import DEBUG
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if DEBUG:
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
